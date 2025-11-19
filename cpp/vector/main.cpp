974603ad810eda13765e3c2679a4488c1c2a03a8
james <james@Frontiers-MacBook-Pro.local>
2025-11-19 15:42:30 +0800

    implement std::vector, placement new blows my mind

---
+++ cpp/vector/main.cpp
@@ -0,0 +1,90 @@
+#include "vector.h"
+#include <vector>
+
+#include <print>
+#include <cassert>
+
+template<typename T>
+class MemAudit {
+private:
+    static std::size_t instance_count;
+    static std::size_t copy_count;
+    static std::size_t move_count;
+
+    std::size_t id_ = 0;
+    T value_{};
+
+public:
+    MemAudit() : id_(++instance_count) {std::println("  [MEM AUDIT] CTOR #{}", id_);}
+
+    MemAudit(const T& value) 
+        : id_(++instance_count) 
+        , value_(value) {
+        std::println("  [MEM AUDIT] CTOR #{} = {}", id_, value_);
+    }
+
+    virtual ~MemAudit() {std::println("  [MEM AUDIT] DTOR #{}", id_);}
+
+    MemAudit(const MemAudit& other) {
+        id_ = other.id_;
+        value_ = other.value_;
+        std::println("  [MEM AUDIT] COPY #{} | {}", id_, ++copy_count);
+    }
+
+    MemAudit(MemAudit&& other) {
+        id_ = other.id_;
+        value_ = other.value_;
+        std::println("  [MEM AUDIT] MOVE #{} | {}", id_, ++move_count);
+    }
+
+    MemAudit<T>& operator=(const MemAudit& other) { 
+        id_ = other.id_;
+        value_ = other.value_;
+        std::println("  [MEM AUDIT] COPY #{} | {}", id_, ++copy_count);
+        return *this;
+    }
+
+    MemAudit<T>& operator=(MemAudit&& other) {
+        id_ = other.id_;
+        value_ = other.value_;
+        std::println("  [MEM AUDIT] MOVE #{} | {}", id_, ++move_count);
+        return *this;     
+    }
+
+    T get() const {
+        std::println("get {}", value_);
+        return value_;
+    }
+};
+
+
+template<typename T> std::size_t MemAudit<T>::instance_count = 0;
+
+template<typename T>
+std::size_t MemAudit<T>::copy_count = 0;
+
+template<typename T>
+std::size_t MemAudit<T>::move_count = 0;
+
+
+int main() {
+    std::println("start of program...");
+    jl::vector<MemAudit<int>> v;
+    v.reserve(10);
+
+    for (int i = 0; i < 5; ++i) {
+        v.emplace_back(i);
+    }
+
+    for (int i = 5; i < 15; ++i) {
+        v.push_back(i);
+    }
+    
+    std::println("running tests...");
+    for (int i = 0; i < v.size(); ++i) {
+        assert(i == v[i].get());
+    }
+    std::println("looks good! goodbye...");
+
+    return 0;
+}
---
+++ cpp/vector/vector.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <cstddef>
+#include <stdexcept>
+#include <print>
+#include <utility>
+
+namespace jl {
+
+template<typename T>
+class vector {
+private:
+
+    T* managed_ptr_       = nullptr;
+    std::size_t size_     = 0;
+    std::size_t capacity_ = 0;
+
+    void delete_managed_ptr() {
+        if (managed_ptr_ == nullptr)
+            return;
+        ::operator delete[](managed_ptr_);
+    }
+
+public:
+
+    vector() {
+        reserve(1); 
+    }
+
+    vector(std::size_t size) : size_(size) {
+        reserve(size);
+        for (size_t i = 0; i < size_; ++i) {
+            managed_ptr_[i] = T{};
+        }
+    }
+
+    ~vector() {
+        delete_managed_ptr();
+    }
+
+    vector(const vector& to_be_copied) {
+    }
+
+    vector(vector&& to_be_moved) {
+
+    }
+
+    vector& operator=(const vector& to_be_copied) {
+    }
+
+    vector& operator=(vector&& to_be_moved) {
+    }
+
+    void reserve(std::size_t desired_capacity) {
+        if (desired_capacity == 0 || capacity_ >= desired_capacity)
+            return;
+
+        capacity_ = desired_capacity;
+
+        T* new_mem_space = static_cast<T*>(::operator new[](sizeof(T) * capacity_));
+        std::println("moving to new mem region...");
+        for (std::size_t i = 0; i < size_; ++i) 
+            new_mem_space[i] = std::move(managed_ptr_[i]);
+        std::println("completed move...");
+
+        delete_managed_ptr();
+        managed_ptr_ = new_mem_space;
+    }
+void push_back(const T& value) {
+        std::println(".push_back()...");
+        if (size_ == capacity_) {
+            expand_capacity();
+        }
+
+        managed_ptr_[size_] = value;
+        size_++;
+    }
+
+    template<typename ...Args>
+    void emplace_back(Args&&... args) {
+        std::println(".emplace_back({})...", args...);
+        if (size_ == capacity_) {
+            expand_capacity();
+        }
+
+        new(managed_ptr_ + size_) T{std::forward<Args>(args)...};
+        size_++;
+    }
+
+    void erase(std::size_t at) {
+        if (at >= size_) {
+            throw std::runtime_error("ERASING OUTSIDE OF VECTOR");
+        }
+
+        --size_;
+        for (std::size_t i = at; i < size_; ++i) {
+            managed_ptr_[i] = managed_ptr_[i+1];
+        }
+    }
+
+    std::size_t size() const {
+        return size_;    
+    }
+
+    std::size_t capacity() const {
+        return capacity_;
+    }
+
+    void shrink_to_fit() {
+    }
+
+    T& operator[](std::size_t index) {
+        return managed_ptr_[index]; 
+    }
+
+private:
+
+    void expand_capacity() {
+        std::println("requested to expand [size: {}, cap: {}]...", size_, capacity_);
+        if (capacity_ == SIZE_MAX) {
+            throw std::runtime_error("VECTOR HIT MAX CAPACITY");
+        } else if (capacity_ >= SIZE_MAX >> 1) {
+            reserve(SIZE_MAX);
+        } else {
+            reserve(capacity_ << 1);
+        }
+    }
+        
+};
+
+}
+
