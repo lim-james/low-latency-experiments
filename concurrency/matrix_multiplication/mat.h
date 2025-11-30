ee1ffdef984cda8eef76582ebb1bc95b5ddc54c0
james <james@Frontiers-MacBook-Pro.local>
2025-11-30 20:28:11 +0800

    move simd mul to tile based operation

--- concurrency/matrix_multiplication/mat.h
+++ concurrency/matrix_multiplication/mat.h
@@ -23,6 +23,10 @@ private:
 
 public:
 
+    SquareMatrix()
+        : matrix_{0}
+        , transposed_{0} {}
+
     static SquareMatrix make_random(T lower_bound, T upper_bound) {
         thread_local std::random_device rd; 
         thread_local std::mt19937 gen(rd()); 
@@ -42,7 +46,7 @@ public:
         return random_matrix;
     }
 
-    void print() {
+    void print() const {
         for (std::size_t y = 0; y < N; ++y) {
             for (std::size_t x = 0; x < N; ++x) {
                 std::print("{:8} ", matrix_[getIndex(x,y)]); 
@@ -66,40 +70,47 @@ public:
         return product;
     }
 
-
     SquareMatrix mul_simd(const SquareMatrix& other) const {
-        SquareMatrix product;
+        SquareMatrix product{};
 
-        simd_t a_vector;
-        simd_t b_vector;
+        constexpr std::size_t TILE_SIZE = 32;
 
-        for (std::size_t y = 0; y < N; ++y) {
-            for (std::size_t x = 0; x < N; ++x) {
-                simd_t vsum(0);
-                std::size_t k = 0;
+        simd_t va, vb;
 
-                auto a_row = matrix_.data() + y * N;
-                auto b_col = other.transposed_.data() + x * N;
+        const T* A = matrix_.data();
+        const T* BT = other.transposed_.data();
+        T* C = product.matrix_.data();
 
-                for (; k + simd_t::size() <= N; k += simd_t::size()) {
-                    simd_t va;
-                    simd_t vb;
+        for (std::size_t ii = 0; ii < N; ii += TILE_SIZE) {
+            for (std::size_t jj = 0; jj < N; jj += TILE_SIZE) {
+                for (std::size_t kk = 0; kk < N; kk += TILE_SIZE) {
 
-                    va.copy_from(a_row + k, stdx::vector_aligned);
-                    vb.copy_from(b_col + k, stdx::vector_aligned);
+                    const std::size_t i_end = std::min(ii + TILE_SIZE, N);
+                    const std::size_t j_end = std::min(jj + TILE_SIZE, N);
+                    const std::size_t k_end = std::min(kk + TILE_SIZE, N);
 
-                    vsum += va * vb;
+                    for (std::size_t i = ii; i < i_end; ++i) {
+                        const T* a_row = A + i*N;
+
+                        for (std::size_t j = jj; j < j_end; ++j) {
+                            simd_t vsum(0);
+
+                            const T* b_row = BT + j*N;
+
+                            for (std::size_t k = kk; k + simd_t::size() <= k_end; k += simd_t::size()) {
+                                va.copy_from(a_row + k, stdx::vector_aligned);
+                                vb.copy_from(b_row + k, stdx::vector_aligned);
+                                vsum += va * vb;
+                            }
+
+                            C[i*N + j] += vsum[0] + vsum[1] + vsum[2] + vsum[3];
+                        }
+                    }
+
                 }
-
-
-                //T sum = 0;
-                //for (std::size_t lane = 0; lane < simd_t::size(); lane++)
-                //    sum += vsum[lane];
-
-                //product.matrix_[getIndex(x,y)] = sum;
-                product.matrix_[getIndex(x,y)] = vsum[0] + vsum[1] + vsum[2] + vsum[3];
             }
         }
+
 
         return product;
     }
