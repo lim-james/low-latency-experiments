2c24bd3e21dcd213ad7e44acaee94b050fca7e2e
james <james@Frontiers-MacBook-Pro.local>
2025-11-30 14:19:22 +0800

    flatten and generalise square matrix implementation

--- concurrency/matrix_multiplication/mat.h
+++ concurrency/matrix_multiplication/mat.h
@@ -1,27 +1,114 @@
 #pragma once
 
 #include <array>
-#include <cstdint>
+#include <random>
+#include <print>
+
 #include <experimental/simd>
 
 namespace stdx = std::experimental;
 
-using value_t = std::int32_t;
-using simd_t  = stdx::native_simd<value_t>;
+template<typename T, std::size_t N>
+class alignas(stdx::memory_alignment_v<stdx::native_simd<T>>) SquareMatrix {
+private:
 
-constexpr std::size_t W = stdx::native_simd<value_t>::size();
-constexpr std::size_t DIM = W * 64;
+    using simd_t = stdx::native_simd<T>;
 
-struct alignas(stdx::memory_alignment_v<simd_t>) aligned_row_t {
-    std::array<value_t, DIM> row;
+    std::array<T, N*N> matrix_;
+    std::array<T, N*N> transposed_;
+
+    constexpr static inline std::size_t getIndex(std::size_t x, std::size_t y) {
+        return y * N + x;
+    }
+
+public:
+
+    static SquareMatrix make_random(T lower_bound, T upper_bound) {
+        thread_local std::random_device rd; 
+        thread_local std::mt19937 gen(rd()); 
+        std::uniform_int_distribution<> distrib(lower_bound, upper_bound); 
+
+        SquareMatrix random_matrix;
+        for (std::size_t i = 0; i < N*N; ++i) 
+            random_matrix.matrix_[i] = distrib(gen);
+
+        for (std::size_t y = 0; y < N; ++y) {
+            for (std::size_t x = 0; x <= y; ++x) {
+                random_matrix.transposed_[getIndex(x,y)] = random_matrix.matrix_[getIndex(y,x)];
+                random_matrix.transposed_[getIndex(y,x)] = random_matrix.matrix_[getIndex(x,y)];
+            }
+        }
+
+        return random_matrix;
+    }
+
+    void print() {
+        for (std::size_t y = 0; y < N; ++y) {
+            for (std::size_t x = 0; x < N; ++x) {
+                std::print("{:8} ", matrix_[getIndex(x,y)]); 
+            }
+            std::println();
+        }
+    }
+
+    SquareMatrix mul_naive(const SquareMatrix& other) const {
+        SquareMatrix product;
+
+        for (std::size_t y = 0; y < N; ++y) {
+            for (std::size_t x = 0; x < N; ++x) {
+                product.matrix_[getIndex(x,y)] = 0;
+                for (std::size_t k = 0; k < N; ++k) {
+                    product.matrix_[getIndex(x,y)] += matrix_[getIndex(k,y)] * other.matrix_[getIndex(x,k)];
+                }
+            }
+        }
+
+        return product;
+    }
+
+
+    SquareMatrix mul_simd(const SquareMatrix& other) const {
+        SquareMatrix product;
+
+        simd_t a_vector;
+        simd_t b_vector;
+
+        for (std::size_t y = 0; y < N; ++y) {
+            for (std::size_t x = 0; x < N; ++x) {
+                simd_t vsum(0);
+                std::size_t k = 0;
+
+                auto a_row = matrix_.data() + y * N;
+                auto b_col = other.transposed_.data() + x * N;
+
+                for (; k + simd_t::size() <= N; k += simd_t::size()) {
+                    simd_t va;
+                    simd_t vb;
+
+                    va.copy_from(a_row + k, stdx::vector_aligned);
+                    vb.copy_from(b_col + k, stdx::vector_aligned);
+
+                    vsum += va * vb;
+                }
+
+
+                //T sum = 0;
+                //for (std::size_t lane = 0; lane < simd_t::size(); lane++)
+                //    sum += vsum[lane];
+
+                //product.matrix_[getIndex(x,y)] = sum;
+                product.matrix_[getIndex(x,y)] = vsum[0] + vsum[1] + vsum[2] + vsum[3];
+            }
+        }
+
+        return product;
+    }
+
+    bool operator==(const SquareMatrix& other) const {
+        for (std::size_t i = 0; i < N*N; ++i)
+            if (matrix_[i] != other.matrix_[i])
+                return false;
+        return true;
+    }
 };
-using int32_mat     = std::array<aligned_row_t, DIM>;
 
-int32_mat make_random_int32_mat(value_t lower_bound, value_t upper_bound);
-
-void print_mat(const int32_mat& mat);
-
-int32_mat transpose(const int32_mat& mat);
-
-int32_mat mul_naive(const int32_mat& a, const int32_mat& b);
-int32_mat mul_simd(const int32_mat& a, const int32_mat& b);
--- concurrency/matrix_multiplication/naive.cpp
+++ concurrency/matrix_multiplication/naive.cpp
@@ -1,45 +1,97 @@
 #include "mat.h"
+
 #include <print>
-
 #include <chrono>
+#include <cassert>
 
-class ScopeTimer {
+template<typename ...Args>
+inline void log_row(Args... args) {
+    std::println("{:5} | {:4} | {:7} | {:7}", args...);
+}
+
+class [[nodiscard]] ScopeTimer {
 private:
-    const char* name_;
+    double* out_;
     std::chrono::time_point<std::chrono::high_resolution_clock> start_;
 
 public:
-    ScopeTimer(const char* name)
-        : name_(name)
+    ScopeTimer(double* out)
+        : out_(out)
         , start_(std::chrono::high_resolution_clock::now()) {}
 
     ~ScopeTimer() {
         auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration<double>(end - start_).count();
-        std::println("{}: {}", name_, duration);
+        *out_ = std::chrono::duration<double, std::milli>(end - start_).count();
     }
 };
 
-int main() {
-    auto a = make_random_int32_mat(1, 100);
-    auto b = make_random_int32_mat(1, 100);
 
+constexpr double calculate_throughput_per_s(double tottime_ms, std::size_t ncalls) {
+    constexpr double ms_to_s = 1'000.0;
+    return static_cast<double>(ncalls) / tottime_ms * ms_to_s; 
+}
 
-    constexpr int ITERATIONS = 1;
+constexpr double calculate_throughput_per_ms(double tottime_ms, std::size_t ncalls) {
+    return static_cast<double>(ncalls) / tottime_ms; 
+
+}
+
+template<std::size_t SCALE, int ITERATIONS>
+void test() {
+    constexpr std::size_t DIM = stdx::native_simd<std::int32_t>::size() * SCALE;
+
+    auto a = SquareMatrix<std::int32_t, DIM>::make_random(1, 10);
+    auto b = SquareMatrix<std::int32_t, DIM>::make_random(1, 10);
+
+    SquareMatrix<std::int32_t, DIM> naive_result;
+    SquareMatrix<std::int32_t, DIM> simd_result;
+
+    double naive_tottime;
+    double simd_tottime;
     
     {
-        auto _ = ScopeTimer("naive");
+        auto _ = ScopeTimer(&naive_tottime);
         for (int i = 0; i < ITERATIONS; ++i) 
-            volatile auto res = mul_naive(a, b);
+            naive_result = a.mul_naive(b);
+
     }
 
-
-    std::println();
     {
-        auto _ = ScopeTimer("simd ");
+        auto _ = ScopeTimer(&simd_tottime);
         for (int i = 0; i < ITERATIONS; ++i) 
-            volatile auto res = mul_simd(a, b);
+            simd_result = a.mul_simd(b);
     }
 
+    const int naive_throughput = std::round(calculate_throughput_per_s(naive_tottime, ITERATIONS));
+    const int simd_throughput  = std::round(calculate_throughput_per_s(simd_tottime,  ITERATIONS));
+
+    log_row(ITERATIONS, DIM, naive_throughput, simd_throughput);
+
+    assert(naive_result == simd_result);
+}
+
+template<
+    std::pair<std::size_t,std::size_t> SCALE, 
+    std::pair<std::size_t,std::size_t> ITERATIONS
+>
+constexpr void test_recursive() {
+    constexpr auto LOWER_SCALE = SCALE.first;
+    constexpr auto UPPER_SCALE = SCALE.second;
+
+    constexpr auto LOWER_ITERATIONS = ITERATIONS.first;
+    constexpr auto UPPER_ITERATIONS = ITERATIONS.second;
+
+    test<LOWER_SCALE, LOWER_ITERATIONS>();
+
+    if constexpr (LOWER_SCALE < UPPER_SCALE) 
+        test_recursive<{LOWER_SCALE*2, UPPER_SCALE}, ITERATIONS>();
+    if constexpr (LOWER_ITERATIONS < UPPER_ITERATIONS) 
+        test_recursive<SCALE, {LOWER_ITERATIONS*10,UPPER_ITERATIONS}>();
+}
+
+int main() {
+    log_row("COUNT", "SIZE", "NAIVE", "SIMD");
+    std::println("--------------------------------");
+    test_recursive<{16,64}, {100,100}>();
     return 0;
 }
